1b7160ac0f18b4a7dc783e19db259b42414ced79
Hüseyin Cem Aras <hcemaras@gmail.com>
2020-06-09 15:59:14 +0300

    index

---
+++ index/code.R
@@ -0,0 +1,9 @@
+install.packages(c("rmarkdown","devtools", "knitr","bookdown"))
+library(rmarkdown)
+library(knitr)
+library(bookdown)
+library(devtools)
+
+install_github("ismayc/thesisdown")
+library(thesisdown)
+
